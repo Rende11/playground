df9c2316d242d95605d45aca9b43bb420926ad4e
Rende11 <artamonovn@gmail.com>
2024-07-26 16:54:59 +0300

    Init kafka java example

---
+++ kafka-java/src/main/java/io/playground/kafka/Producer.java
@@ -0,0 +1,11 @@
+package io.playground.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Producer {
+    private static final Logger log = LoggerFactory.getLogger(Producer.class);
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+    }
+}
