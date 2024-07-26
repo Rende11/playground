62c246313bdfcd6ab588d224fd47190a8885c9a3
Rende11 <artamonovn@gmail.com>
2024-07-26 17:05:33 +0300

    Demo producer

--- kafka-java/src/main/java/io/playground/kafka/Producer.java
+++ kafka-java/src/main/java/io/playground/kafka/Producer.java
@@ -1,11 +1,35 @@
 package io.playground.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
 
 public class Producer {
     private static final Logger log = LoggerFactory.getLogger(Producer.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        log.info("Launch Kafka Producer");
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", "hello kafka world");
+
+        producer.send(producerRecord);
+
+        producer.flush();
+
+        producer.close();
     }
+
 }
